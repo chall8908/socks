47d542a79b7ddcb1e4b17ef954e85d37a379bcdf
Chris Hall <chall@pangeare.com>
2014-10-21 15:56:32 -0500

    Update main file to be less browser reliant

--- socks.js
+++ socks.js
@@ -1,5 +1,11 @@
 (function() {
-  "use strict";
+  'use strict';
+
+  if(typeof(WebSocket) == 'undefined') {
+    // Well, this is awkward....
+    console.log("Sorry, your current browser doesn't support WebSockets");
+    return;
+  }
   
   var WS = WebSocket;
 
@@ -17,14 +23,14 @@
     // Do some quick type checking to ensure that the type of data we're sending
     // is supported by the send method.  If it's not, we'll try to stringify it.
     if(
-      !(window.ArrayBuffer && data instanceof ArrayBuffer) &&
-      !(window.Blob && data instanceof Blob) &&
-      typeof(data) != "string"
+      !(typeof(ArrayBuffer) != "undefined" && data instanceof ArrayBuffer) &&
+      !(typeof(Blob)        != "undefined" && data instanceof Blob) &&
+      typeof(data) != 'string'
     ) {
       try {
         data = JSON.stringify(data);
       } catch(e) {
-        throw new Error("Unable to send data with type, " + typeof(data));
+        throw new Error('Unable to send data with type, ' + typeof(data));
       }
     }
 
@@ -39,13 +45,13 @@
    * @param {String} url - the URL or path to connect to
    */
   function Sock(url) {
-    if(url[0] === "/") {
+    if(url[0] === '/') {
       // our URL is really a path.  Gotta make it absolute
-      url = (location.protocol === "https:" ? "wss://" : "ws://") + location.host + url;
+      url = (location.protocol === 'https:' ? 'wss://' : 'ws://') + location.host + url;
     }
 
     return new WS(url);
   }
 
-  window.WebSocket = Sock;
+  WebSocket = Sock;
 }());
